e9bef358f626d85a8b8747816b5a2c683ed15de7
LALLEMENT Jaufré <J.LALLEMENT@gds.local>
2022-06-23 12:18:15 +0200

    Add: implemented time diff function

--- KPIRetreiver/private/Get-TimeDiff.ps1
+++ KPIRetreiver/private/Get-TimeDiff.ps1
@@ -1,6 +1,7 @@
 function Get-TimeDiff ([datetime]$t1, [datetime]$t2) {
   $rawDiff = $t2 - $t1
 
+  # Time properties we want to retreive from the above substraction
   $timeProps = @(
     [pscustomobject]@{ name = 'Days'        ; abr = 'd'  },
     [pscustomobject]@{ name = 'Hours'       ; abr = 'h'  },
@@ -9,11 +10,12 @@ function Get-TimeDiff ([datetime]$t1, [datetime]$t2) {
     [pscustomobject]@{ name = 'Milliseconds'; abr = 'ms' }
   )
 
-  $diff = @()
+  # For each property if not zero : add it to the stack
+  $diffStack = @()
   foreach ($p in $timeProps) {
     $pName, $pAbr = $p.name, $p.abr
-    if ($rawDiff.$pName -gt 0) { $diff += "$($rawDiff.$pName)$pAbr" }
+    if ($rawDiff.$pName -gt 0) { $diffStack += "$($rawDiff.$pName)$pAbr" }
   }
 
-  return ($diff -join ', ')
+  return ($diffStack -join ', ')
 }
