635a4739c59c48f8f4f9fcb0d1463dd733e33a0a
Jaufré Lallement <lallement.j.pro@outlook.fr>
2022-10-21 15:51:10 +0200

    Add: placed help string in separate file

---
+++ KPIRetreiver/public/Help.ps1
@@ -0,0 +1,16 @@
+$helpInfos = @"
+Usage:
+  Invoke-KPIRetreiver (-Output) [-Extracts]
+  Invoke-KPIRetreiver -Help
+  Invoke-KPIRetreiver (-Version)
+  
+Options:
+  -Extracts                      provide an extract from AD if you have one
+  -Help                          show this help message and exit
+  -Output                        set the output path
+  -Version                       show version and exit
+
+Help:
+  For help using this tool, please open an issue on the Github repository:
+  https://github.com/Melk0rr/Odysseus
+"@
